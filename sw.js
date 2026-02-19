668ded5563cfe7f0b3c84256519d7c0a720d2474
ewertonantonio100-commits <ewertonantonio100@gmail.com>
2026-02-19 15:41:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'authon-cache-v6';
+const CACHE_NAME = 'authon-cache-v7';
 const urlsToCache = [
   'app.html', // <--- AQUI ESTÁ O SEGREDO! Salvamos o app, não a página de vendas
   'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Oswald:wght@500;700&display=swap',

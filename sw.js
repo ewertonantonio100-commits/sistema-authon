8d17e87ebb88ffcc41ed4b5934f3e7d32312eb0f
ewertonantonio100-commits <ewertonantonio100@gmail.com>
2026-02-19 12:37:24 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'authon-cache-v2';
+const urlsToCache = [
+  'app.html', // <--- AQUI ESTÁ O SEGREDO! Salvamos o app, não a página de vendas
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Oswald:wght@500;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+// Instala o Service Worker e salva os arquivos estáticos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Intercepta as requisições para abrir mais rápido ou funcionar offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualiza o cache quando você lançar uma versão nova
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});

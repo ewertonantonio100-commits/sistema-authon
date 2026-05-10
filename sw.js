76b76773dd9a866358edd55c9e48b1dbfe466ad9
ewertonantonio100-commits <ewertonantonio100@gmail.com>
2026-05-10 11:36:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Para forçar atualização: suba arquivos novos no GitHub
 // ============================================================
 
-const CACHE_VERSION = 'authon-cache-v121';
+const CACHE_VERSION = 'authon-cache-v122';
 
 const urlsToCache = [
   // ── Fontes e ícones externos ──
